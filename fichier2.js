c83b6470552a358a319b9871e7c576f45abbad80
AbdourahimMbodji <rahimmbodj@gmail.com>
2023-10-19 19:40:09 +0000

    Deuxieme commit

---
+++ fichier2.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemple de script JavaScript</title>
+</head>
+<body>
+    <p>Hello, World!</p>
+
+    <script>
+        // Votre code JavaScript va ici
+        console.log("Ceci est un exemple de script JavaScript");
+    </script>
+</body>
+</html>
